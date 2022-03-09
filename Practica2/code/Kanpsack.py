8cbcb4d08ed76242c9cdbe77bdd8e29d5a5a3f1b
Rafael Egea Jurado <55484111+i82egjur@users.noreply.github.com>
2022-03-09 10:14:11 +0100

    Update Kanpsack.py

--- Practica2/code/Kanpsack.py
+++ Practica2/code/Kanpsack.py
@@ -1,4 +1,6 @@
-import random
+import random, csv
+import matplotlib.pyplot as plt
+import pandas as pd
 
 def evaluarSolucion(solucion, precios, pesos, pesoMax):
     precio = 0
@@ -25,15 +27,13 @@ def aplicarOperadoresGeneticos(poblacion, k, cProb, mProb):
 
     return poblacion #Devolver la nueva poblacion (sin evaluar)
 
-def main():
-    pesos = [ 34, 45, 14, 76, 32 ]
-    precios = [ 340, 210, 87, 533, 112 ]
-    pesoMax = 100 #Peso máximo que se puede poner en la mochila
-    nSoluciones = 25 #Tamaño de la poblacion
-    maxGeneraciones = 1 #Numero de generaciones
-    k = 3 #Tamaño torneo selector de padres
-    cProb = 0.7 #Probabilidad de cruce
-    mProb = 0.1 #Probabilidad de mutacion
+def ejecutar(pesos,precios,pesoMax,nSoluciones,maxGeneraciones,cProb,mProb):
+	pesoMax = parametros[0]
+	nSoluciones = parametros[1]
+	maxGeneraciones = parametros[2]
+	cProb = parametros[3]
+	mProb = parametros[4]
+
 
     l=len(pesos)
     ##Creamos n soluciones aleatorias que sean válidas
@@ -64,6 +64,78 @@ def main():
         for solucion in nSoluciones:
             poblacion.append([solucion[0],evaluarSolucion(solucion[0],precios,pesos,pesoMax)])
         it+=1
+    peorSolucion, SoluMedia, mejorSolu = 0,0,0
+    return peorSolucion, SoluMedia, mejorSolu
+
+def main():
+    pesos = [ 34, 45, 14, 76, 32 ]
+    precios = [ 340, 210, 87, 533, 112 ]
+    pesoMax = 100 #Peso máximo que se puede poner en la mochila
+    nSoluciones = 25 #Tamaño de la poblacion
+    maxGeneraciones = 1 #Numero de generaciones
+    k = 3 #Tamaño torneo selector de padres
+    cProb = 0.7 #Probabilidad de cruce
+    mProb = 0.1 #Probabilidad de mutacion
+
+    parametros = [pesoMax, nSoluciones, maxGeneraciones, k, cProb, mProb]
+
+    '''
+	    setNSoluciones = [5, 10, 15, 20, 25, 30]
+	    setTamaniosTorneo = [1, 2, 4, 5, 10, 25]
+	    setMaxGeneraciones = [1, 2, 3, 4, 5, 6]
+	    setProbabilidadDeCruce = [0.1, 0.3, 0.5, 0.6, 0.7]
+	    setProbabilidadDeMutacion = [0.1, 0.3, 0.5, 0.6, 0.7]	
+    '''
+    nombreMediciones = ["Numero de soluciones", "Tamaño del torneo", "Maximo generaciones", "Probabilidad de cruce", "Probabilidad de mutacion"]
+
+    valoresDistintosValoresMediciones = [   [5, 10, 15, 20, 25, 30], 
+										    [1, 2, 4, 5, 10, 25], 
+										    [1, 2, 3, 4, 5, 6], 
+										    [0.1, 0.3, 0.5, 0.6, 0.7] ]
+										    [0.1, 0.3, 0.5, 0.6, 0.7], 
+
+    for nMedicion in range(len(nombreMediciones)):
+    	with open(nombreMediciones[nMedicion], 'w', newline='') as csvfile:
+       		spamwriter = csv.writer(csvfile, delimiter='|', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+	        spamwriter.writerow([nombreMediciones[nMedicion], "Peor solución", "Solución media", "Mejor solución"])
+	    	
+	    	for valor in range(0, len(valoresDistintosValoresMediciones[nMedicion]))
+	    		parametros[nMedicion] = valoresDistintosValoresMediciones[nMedicion][valor]
+	    		peorSolucion, SoluMedia, mejorSolu = ejecutar(pesos, precios, parametros)
+	            spamwriter.writerow([valoresDistintosValoresMediciones[nMedicion], peorSolucion, SoluMedia, mejorSolu])
+
+		data = pd.read_csv(nombreMediciones[nMedicion])
+		df = pd.DataFrame(data)
+		X = list(df.iloc[:, 0])
+		Y = list(df.iloc[:, 1])
+		plt.bar(X, Y, color='g')
+		plt.title(nombreMediciones[nMedicion])
+		plt.xlabel("Variación parámetros")
+		plt.ylabel("Soluciones")
+		plt.savefig(nombreMediciones[nMedicion]+".png")
+
+
 
 if __name__ == "__main__":
     main()
+
+
+
+
+  
+  
+# Initialize the lists for X and Y
+  
+df = pd.DataFrame(data)
+  
+X = list(df.iloc[:, 0])
+Y = list(df.iloc[:, 1])
+  
+# Plot the data using bar() method
+plt.bar(X, Y, color='g')
+plt.title("Students over 11 Years")
+plt.xlabel("Years")
+plt.ylabel("Number of Students")
+  
+# Show the plot
+plt.show()
